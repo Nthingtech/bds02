5871c8e6f48bcd324bc6c976b293908f5552da05
Fernando Lopes dos Santos <store.nthing@gmail.com>
2021-12-11 21:01:06 -0300

    Event update correction TDD

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
 import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
@@ -16,6 +18,9 @@ public class EventService {
 
 	@Autowired
 	private EventRepository repository;
+	
+	@Autowired
+	private CityRepository cityRepository;
 
 	
 	
@@ -23,10 +28,12 @@ public class EventService {
 	public EventDTO update(Long id, EventDTO dto) {
 		try {
 			Event entity = repository.getOne(id);
+			City entityCity = cityRepository.getOne(dto.getCityId());
+			
 			entity.setName(dto.getName());
 			entity.setDate(dto.getDate());
 			entity.setUrl(dto.getUrl());
-			entity.setCity(null);;
+			entity.setCity(entityCity);
 			entity = repository.save(entity);
 			return new EventDTO(entity);
 		}
